eb820de1815669e5f121d678bce504395566ab4d
ssnepenthe <ssnepenthe@users.noreply.github.com>
2018-10-04 08:10:07 -0700

    Remove auth and logged in cookies before saving data.
    Applies to $_COOKIE array as well as cookie headers in $_SERVER.
    
    Closes #12.

--- src/Data_Sources/class-php.php
+++ src/Data_Sources/class-php.php
@@ -8,9 +8,42 @@ class Php extends PhpDataSource {
 	public function removePasswords( array $data ) {
 		$keys = array_keys( $data );
 		$values = array_map( function( $value, $key ) {
-			return preg_match( '/pass|pwd/i', $key ) === 1 ? '*removed*' : $value;
+			$cookies = [ AUTH_COOKIE, SECURE_AUTH_COOKIE, LOGGED_IN_COOKIE ];
+
+			if (
+				// @todo Filterable pattern(s) for data to remove?
+				preg_match( '/pass|pwd/i', $key ) === 1
+				|| preg_match( '/' . implode( '|', $cookies ) . '/i', $key ) === 1
+			) {
+				return '*removed*';
+			}
+
+			return $value;
 		}, $data, $keys );
 
 		return array_combine( $keys, $values );
+	}
+
+	protected function getRequestHeaders() {
+		$headers = parent::getRequestHeaders();
+
+		// We are removing sensitive data from the $_COOKIE array elsewhere...
+		// Lets make sure to parse the cookie headers so we can remove the same data.
+		$headers['Cookie'] = array_map( function( $cookie_string ) {
+			$clean = array_map( function( $pair ) {
+				list( $key, $value ) = explode( '=', trim( $pair ) );
+
+				$clean = $this->removePasswords( [ $key => $value ] );
+
+				$key = key( $clean );
+				$value = current( $clean );
+
+				return "{$key}={$value}";
+			}, explode( ';', $cookie_string ) );
+
+			return implode( '; ', $clean );
+		}, isset( $headers['Cookie'] ) ? $headers['Cookie'] : [] );
+
+		return $headers;
 	}
 }
