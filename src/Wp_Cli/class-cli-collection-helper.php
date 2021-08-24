7cfb3ff9df389a52506d0a6d7e6b571cc16b0c25
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-08-24 13:31:24 -0700

    Override ob_start_callback on execution logger
    We were silently discarding a bunch of directly echoed output previously. This fixes it

--- src/Wp_Cli/class-cli-collection-helper.php
+++ src/Wp_Cli/class-cli-collection-helper.php
@@ -8,26 +8,16 @@ use WP_CLI;
 use WP_CLI\Loggers\Execution;
 
 class Cli_Collection_Helper {
-	protected $events;
 	protected $logger;
-
-	public function __construct( Event_Manager $events ) {
-		$this->events = $events;
-	}
 
 	public function initialize_logger() {
 		// Replace the logger immediately.
 		$this->replace_wp_cli_logger();
-
-		// And flush on the latest non-shutdown hook.
-		$this->events->on( 'wp_loaded', function() {
-			$this->get_logger()->ob_end();
-		}, Event_Manager::LATE_EVENT );
 	}
 
 	public function get_logger() {
 		if ( null === $this->logger ) {
-			$this->logger = new Execution( WP_CLI::get_runner()->in_color() );
+			$this->logger = new Recorder( WP_CLI::get_runner()->in_color() );
 			$this->logger->ob_start();
 		}
 
@@ -37,20 +27,19 @@ class Cli_Collection_Helper {
 	public function replace_wp_cli_logger() {
 		// Method doesn't exist before https://github.com/wp-cli/wp-cli/commit/f854c9678604a405a38e26948344e0a5e75ec1c7.
 		if ( \method_exists( WP_CLI::class, 'get_logger' ) ) {
-			$current_logger = WP_CLI::get_logger();
+			$previous_logger = WP_CLI::get_logger();
 		} else {
 			$ref = new ReflectionProperty( WP_CLI::class, 'logger' );
 			$ref->setAccessible( true );
 
-			$current_logger = $ref->getValue();
+			$previous_logger = $ref->getValue();
 		}
 
-		// @todo ???
-		if ( $current_logger instanceof Execution || $current_logger instanceof Logger_Chain ) {
+		if ( $previous_logger instanceof Logger_Chain ) {
 			return;
 		}
 
-		$new_logger = new Logger_Chain( $current_logger, $this->get_logger() );
+		$new_logger = new Logger_Chain( $previous_logger, $this->get_logger() );
 
 		WP_CLI::set_logger( $new_logger );
 	}
---
+++ src/Wp_Cli/class-recorder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Clockwork_For_Wp\Wp_Cli;
+
+use WP_CLI;
+use WP_CLI\Loggers\Execution;
+
+class Recorder extends Execution {
+	public function ob_start_callback( $str ) {
+		$this->write( STDOUT, $str );
+		return $str;
+	}
+}
