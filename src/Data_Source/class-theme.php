02f215da1e4077c035d6e496a13405b28153df55
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-09-08 15:39:09 -0700

    Theme types for psalm

--- src/Data_Source/class-theme.php
+++ src/Data_Source/class-theme.php
@@ -11,17 +11,49 @@ use Clockwork_For_Wp\Event_Management\Subscriber;
 use Clockwork_For_Wp\Included_Files;
 
 final class Theme extends DataSource implements Subscriber {
+	/**
+	 * @var string[]
+	 */
 	private $body_classes = [];
+
+	/**
+	 * @var null|int
+	 */
 	private $content_width;
+
+	/**
+	 * @var null|string
+	 */
 	private $included_template;
+
+	/**
+	 * @var string[]
+	 */
 	private $included_template_parts = [];
+
+	/**
+	 * @var bool
+	 */
 	private $is_child_theme = false;
+
+	/**
+	 * @var null|string
+	 */
 	private $stylesheet;
+
+	/**
+	 * @var null|string
+	 */
 	private $template;
+
 	/**
 	 * @var string[]
 	 */
 	private $template_hierarchy = [];
+
+	/**
+	 * @var null|string
+	 */
 	private $theme_root;
 
 	public function add_hierarchy_templates( array $templates ): self {
@@ -32,31 +64,62 @@ final class Theme extends DataSource implements Subscriber {
 
 	public function get_subscribed_events(): array {
 		return [
-			'cfw_pre_resolve' => function ( $content_width ): void {
-				$this
-					->set_theme_root( \get_theme_root() )
-					->set_is_child_theme( \is_child_theme() )
-					->set_template( \get_template() )
-					->set_stylesheet( \get_stylesheet() )
-					->set_content_width( $content_width )
-					->set_included_template_parts(
-						...\array_merge(
-							Included_Files::template_parts_from_parent_theme(),
-							Included_Files::template_parts_from_child_theme()
-						)
-					);
-			},
+			'cfw_pre_resolve' => [
+				/**
+				 * @param int $content_width
+				 */
+				function ( $content_width ): void {
+					/**
+					 * @psalm-suppress RedundantCastGivenDocblockType
+					 */
+					$recorded_content_width = (int) $content_width;
+
+					$this
+						->set_theme_root( \get_theme_root() )
+						->set_is_child_theme( \is_child_theme() )
+						->set_template( \get_template() )
+						->set_stylesheet( \get_stylesheet() )
+						->set_content_width( $recorded_content_width )
+						->set_included_template_parts(
+							...\array_merge(
+								Included_Files::template_parts_from_parent_theme(),
+								Included_Files::template_parts_from_child_theme()
+							)
+						);
+				},
+			],
 			'body_class' => [
+				/**
+				 * @param array $classes
+				 *
+				 * @return array $classes
+				 */
 				function ( $classes ) {
-					$this->set_body_classes( \is_array( $classes ) ? $classes : [] );
+					/**
+					 * @psalm-suppress RedundantConditionGivenDocblockType
+					 * @psalm-suppress DocblockTypeContradiction
+					 */
+					$recorded_classes = \is_array( $classes ) ? $classes : [];
+
+					$this->set_body_classes( $recorded_classes );
 
 					return $classes;
 				},
 				Event_Manager::LATE_EVENT,
 			],
 			'template_include' => [
+				/**
+				 * @param string $template
+				 *
+				 * @return string
+				 */
 				function ( $template ) {
-					$this->set_included_template( $template );
+					/**
+					 * @psalm-suppress RedundantCastGivenDocblockType
+					 */
+					$recorded_template = (string) $template;
+
+					$this->set_included_template( $recorded_template );
 
 					return $template;
 				},
@@ -76,9 +139,12 @@ final class Theme extends DataSource implements Subscriber {
 							 */
 							function ( $templates ) {
 								/**
-								 * @psalm-suppress RedundantCastGivenDocblockType
+								 * @psalm-suppress RedundantConditionGivenDocblockType
+								 * @psalm-suppress DocblockTypeContradiction
 								 */
-								$this->add_hierarchy_templates( (array) $templates );
+								$recorded_templates = \is_array( $templates ) ? $templates : [];
+
+								$this->add_hierarchy_templates( $recorded_templates );
 
 								return $templates;
 							},
@@ -90,7 +156,7 @@ final class Theme extends DataSource implements Subscriber {
 		];
 	}
 
-	public function resolve( Request $request ) {
+	public function resolve( Request $request ): Request {
 		$panel = $request->userData( 'Theme' );
 
 		$table = \array_filter(
@@ -162,25 +228,25 @@ final class Theme extends DataSource implements Subscriber {
 		return $request;
 	}
 
-	public function set_body_classes( array $body_classes ) {
+	public function set_body_classes( array $body_classes ): self {
 		$this->body_classes = \array_values( \array_map( 'strval', $body_classes ) );
 
 		return $this;
 	}
 
-	public function set_content_width( int $content_width ) {
+	public function set_content_width( int $content_width ): self {
 		$this->content_width = $content_width;
 
 		return $this;
 	}
 
-	public function set_included_template( string $included_template ) {
+	public function set_included_template( string $included_template ): self {
 		$this->included_template = $included_template;
 
 		return $this;
 	}
 
-	public function set_included_template_parts( string ...$template_parts ) {
+	public function set_included_template_parts( string ...$template_parts ): self {
 		$this->included_template_parts = $template_parts;
 
 		return $this;
@@ -238,6 +304,10 @@ final class Theme extends DataSource implements Subscriber {
 	}
 
 	private function theme_relative_path( string $path ): string {
+		if ( null === $this->theme_root ) {
+			return $path;
+		}
+
 		return \ltrim( \str_replace( $this->theme_root, '', $path ), '/' );
 	}
 }
