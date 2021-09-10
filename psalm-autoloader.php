1256f4253a71bb31f40aa141e2d75e78f587c27b
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-09-10 12:40:50 -0700

    Ensure clock function is loaded for psalm

--- psalm-autoloader.php
+++ psalm-autoloader.php
@@ -4,6 +4,7 @@
 // This is to prevent a bunch of UndefinedFunction errors until I figure out how it is supposed to be done.
 // phpcs:ignoreFile
 
+require_once __DIR__ . '/src/clock.php';
 require_once __DIR__ . '/src/instance.php';
 require_once __DIR__ . '/src/plugin-helpers.php';
 require_once __DIR__ . '/src/wordpress-helpers.php';
