4e0fdab957d2e94234358c6cbb93400ac35ebf96
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-12-08 12:09:28 -0700

    Initialize cypress and move to tests directory

---
+++ tests/cypress/integration/sample_spec.js
@@ -0,0 +1,13 @@
+describe('My First Test', () => {
+    it('Finds the content "type"', () => {
+        cy.visit('https://example.cypress.io');
+
+        cy.contains('type').click();
+
+        cy.url().should('include', '/commands/actions');
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com');
+    });
+});
