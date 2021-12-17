81f3afbf4938a23e4a53328dcdf84ff0354e169e
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-12-17 10:02:17 -0700

    Remove phpunit browser test suite

--- tests/helpers.php
+++ tests/helpers.php
@@ -2,63 +2,6 @@
 
 namespace Clockwork_For_Wp\Tests;
 
-use Symfony\Component\HttpClient\Exception\TransportException;
-
 function fixture_path( $file ) {
 	return __DIR__ . "/fixtures/{$file}";
-}
-
-class Api {
-	protected $ajax_url;
-	protected $client;
-
-	public function __construct( $client ) {
-		$this->client = $client;
-	}
-
-	public function ajax_url() {
-		if ( ! is_string( $this->ajax_url ) ) {
-			try {
-				$this->ajax_url = trim(
-					$this->client
-						->request( 'GET', '/?enable=0' )
-						->filter( '#cfw-coh-ajaxurl' )
-						->text( '' )
-				);
-			} catch ( TransportException $e ) {
-				$this->ajax_url = '';
-			}
-		}
-
-		return $this->ajax_url;
-	}
-
-	public function clean_metadata() {
-		$this->client->request(
-			'GET',
-			"{$this->ajax_url()}?action=cfw_coh_clean_metadata"
-		);
-	}
-
-	public function is_available() {
-		return '' !== $this->ajax_url();
-	}
-
-	public function metadata_count() {
-		$this->client->request(
-			'GET',
-			"{$this->ajax_url()}?action=cfw_coh_metadata_count"
-		);
-
-		return json_decode( $this->client->getResponse()->getContent(), true )['data'];
-	}
-
-	public function metadata_by_id( $id ) {
-		$this->client->request(
-			'GET',
-			"{$this->ajax_url()}?action=cfw_coh_metadata_by_id&id={$id}"
-		);
-
-		return json_decode( $this->client->getResponse()->getContent(), true )['data'];
-	}
 }
