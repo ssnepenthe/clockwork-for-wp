2d38547a17cd9310dab7946c44dd8138edda5420
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-12-17 16:45:29 -0700

    Ensure test helper DB config is cleared before and after tests run

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -21,24 +21,34 @@ import './commands'
 // require('./commands')
 
 before(() => {
+
     cy.visit({
         url: '/?enable=0',
         log: false,
     })
-        .get('[data-cy="test-context"]', {log: false})
-        .invoke({log: false}, 'text')
-        .then(context => {
-            let parsedContext = JSON.parse(context);
+    .get('[data-cy="test-context"]', {log: false})
+    .invoke({log: false}, 'text')
+    .then(context => {
+        let parsedContext = JSON.parse(context);
 
-            cy.task('setTestContext', parsedContext, {log: false})
+        cy.task('setTestContext', parsedContext, {log: false})
 
-            Cypress.log({
-                name: 'setTestContext',
-                message: '',
-                consoleProps: () => ({context: parsedContext}),
-            });
+        Cypress.log({
+            name: 'setTestContext',
+            message: '',
+            consoleProps: () => ({context: parsedContext}),
         });
+    });
+
+    cy.resetConfig();
+
 });
 
 beforeEach(() => cy.cleanRequests());
-after(() => cy.cleanRequests());
+
+after(() => {
+
+    cy.resetConfig();
+    cy.cleanRequests()
+
+});
