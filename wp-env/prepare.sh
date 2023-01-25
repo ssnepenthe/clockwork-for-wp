19f7008dc21066fe43b0cc5b4a82d2988361baf6
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-01-25 14:06:09 -0700

    Re-order commands in wp-env/prepare.sh

--- wp-env/prepare.sh
+++ wp-env/prepare.sh
@@ -3,8 +3,8 @@
 npx wp-env start
 npx wp-env clean all
 npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html"
+npx wp-env run tests-wordpress "mkdir -p /var/www/html/wp-content/cfw-data"
+npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html/wp-content/cfw-data"
 npx wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard"
 # @todo composer flags? --ansi --no-interaction --no-progress --prefer-dist
 npx wp-env run composer install
-npx wp-env run tests-wordpress "mkdir -p /var/www/html/wp-content/cfw-data"
-npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html/wp-content/cfw-data"
