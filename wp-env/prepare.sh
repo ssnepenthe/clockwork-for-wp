99346dacf72e74614687a977d4c638aed12b2468
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-01-27 12:34:45 -0700

    Ensure wp-env setup tasks only run on first use

--- wp-env/prepare.sh
+++ wp-env/prepare.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+previously_started="no"
+
+npx wp-env install-path > /dev/null 2>&1
+
+test $? -eq 0 && previously_started="yes"
+
+echo "Starting WordPress development environment"
+
 npx wp-env start
-npx wp-env clean all
-npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html"
-npx wp-env run tests-wordpress "mkdir -p /var/www/html/wp-content/cfw-data"
-npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html/wp-content/cfw-data"
-npx wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard"
-# @todo composer flags? --ansi --no-interaction --no-progress --prefer-dist
-npx wp-env run composer install
+
+if [[ $previously_started == 'no' ]]; then
+    echo "Performing additional set up tasks..."
+
+    npx wp-env clean all
+    npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html"
+    npx wp-env run tests-wordpress "mkdir -p /var/www/html/wp-content/cfw-data"
+    npx wp-env run tests-wordpress "chmod -c ugo+w /var/www/html/wp-content/cfw-data"
+    npx wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+    # @todo composer flags? --ansi --no-interaction --no-progress --prefer-dist
+    npx wp-env run composer install
+fi
